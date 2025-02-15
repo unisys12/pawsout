6cefa7356801bf6465e8cb9a82be55bbd7f2116a
unisys12 <unisys12@gmail.com>
2025-02-14 19:47:49 -0600

    add missing return type

--- database/migrations/2025_02_14_233057_create_statuses_table.php
+++ database/migrations/2025_02_14_233057_create_statuses_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('statuses', function (Blueprint $table) {
+        Schema::create('statuses', function (Blueprint $table): void {
             $table->id();
             $table->string('label');
             $table->timestamps();
