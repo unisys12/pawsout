c983bcda5cc40d7fe0239b243d72193fc7a4bfa8
Phillip Jackson <unisys12@gmail.com>
2025-03-08 16:05:23 -0600

    feat: create login form with test

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 
+// Public routes
 Route::get('/', function () {
     return inertia('Landing');
+});
+
+// Auth Routes
+Route::get('/login', function () {
+    return inertia('Auth/login');
 });
