01c4f3ccd3c7c1b8b844e6d183f006b0c0c3a575
Phillip Jackson <unisys12@gmail.com>
2025-03-08 16:14:03 -0600

    added dark mode classes

--- resources/js/Pages/Auth/login.jsx
+++ resources/js/Pages/Auth/login.jsx
@@ -4,7 +4,7 @@ export default function Page() {
   return (
     <div className="bg-slate-200 dark:bg-gray-900">
         <header className="flex items-center justify-center">
-            <h1 className="text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Paws Out!</h1>
+            <h1 className="text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-slate-200">Paws Out!</h1>
         </header>
         <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
             <div className="w-full max-w-sm">
--- resources/js/components/login-form.jsx
+++ resources/js/components/login-form.jsx
@@ -18,8 +18,8 @@ export function LoginForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle>Login to your account</CardTitle>
-          <CardDescription>
+          <CardTitle className={cn("dark:text-slate-200")}>Login to your account</CardTitle>
+          <CardDescription className={cn("dark:text-slate-200")}>
             Enter your email below to login to your account
           </CardDescription>
         </CardHeader>
@@ -27,32 +27,32 @@ export function LoginForm({
           <form>
             <div className="flex flex-col gap-6">
               <div className="grid gap-3">
-                <Label htmlFor="email">Email</Label>
+                <Label className={cn("dark:text-slate-200")} htmlFor="email">Email</Label>
                 <Input id="email" type="email" placeholder="m@example.com" required />
               </div>
               <div className="grid gap-3">
                 <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
+                  <Label className={cn("dark:text-slate-200")} htmlFor="password">Password</Label>
                   <a
                     href="#"
-                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline">
+                    className="ml-auto inline-block text-sm underline-offset-4 hover:underline dark:text-slate-200">
                     Forgot your password?
                   </a>
                 </div>
                 <Input id="password" type="password" required />
               </div>
               <div className="flex flex-col gap-3">
-                <Button type="submit" className="w-full">
+                <Button type="submit" className="w-full dark:text-slate-200">
                   Login
                 </Button>
-                <Button variant="outline" className="w-full">
+                <Button variant="outline" className="w-full dark:text-slate-200">
                   Login with Google
                 </Button>
               </div>
             </div>
-            <div className="mt-4 text-center text-sm">
+            <div className="mt-4 text-center text-sm dark:text-slate-200">
               Don&apos;t have an account?{" "}
-              <a href="#" className="underline underline-offset-4">
+              <a href="#" className="underline underline-offset-4 dark:text-slate-200">
                 Sign up
               </a>
             </div>
